19af80e8a78bd90f2f083fb9035c50f9e7dfccab
ViktarYeltsou <78786714+ViktarYeltsou@users.noreply.github.com>
2021-07-12 23:01:48 +0300

    refactor:

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-//module.exports = function toReadable (number) {
-  
-//}
-
-const units = ['','one','two','three','four','five','six','seven','eight','nine'];
-const tens = ['ten', 'eleven', 'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-const tws = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+module.exports = function toReadable (number) {
+    const units = ['','one','two','three','four','five','six','seven','eight','nine'];
+    const tens = ['ten', 'eleven', 'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const tws = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
 // const over = ['hundred','thousand','million','billion'];
 
-const toReadable = num => !~~(num / 100) ? lessHundred(num) : units[~~(num / 100)] + ' hundred ' + lessHundred(num % 100);
+//const toReadable = num => !~~(num / 100) ? lessHundred(num) : units[~~(num / 100)] + ' hundred ' + lessHundred(num % 100);
 
-const lessHundred = num => num < 10 ? units[num] : num <20 ? tens[num % 10] : tws[~~(num / 10)] + ' ' + units[num % 10];
+    const lessHundred = num => num < 10 ? units[num] : num <20 ? tens[num % 10] : tws[~~(num / 10)] + ' ' + units[num % 10];
+
+  return !~~(number / 100) ? lessHundred(number) : units[~~(number / 100)] + ' hundred ' + lessHundred(number % 100);
+}
+
+//const units = ['','one','two','three','four','five','six','seven','eight','nine'];
+//const tens = ['ten', 'eleven', 'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+//const tws = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+// const over = ['hundred','thousand','million','billion'];
+
+//const toReadable = num => !~~(num / 100) ? lessHundred(num) : units[~~(num / 100)] + ' hundred ' + lessHundred(num % 100);
+
+//const lessHundred = num => num < 10 ? units[num] : num <20 ? tens[num % 10] : tws[~~(num / 10)] + ' ' + units[num % 10];//
